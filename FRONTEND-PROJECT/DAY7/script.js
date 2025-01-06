5bd359f2bbc18b4890bdac8cf2605f577ef80066
Aishwaryak12-04 <aishwaryak1204@gmail.com>
2025-01-06 21:25:32 +0530

    javascript assignment

--- FRONTEND-PROJECT/DAY7/script.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.getElementsByClassName('header');
-    
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 350) {
-        navbar[0].classList.add('scrolled');
-    } else {
-        navbar[0].classList.remove('scrolled');
-    }
-});
