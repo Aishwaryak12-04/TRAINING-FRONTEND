ea8b3462fdf6b3960a4e0c7f57a1d392313ec4a8
Aishwaryak12-04 <aishwaryak1204@gmail.com>
2025-01-08 22:50:29 +0530

    gaming website

--- FRONTEND-PROJECT/DAY6/demo.js
+++ FRONTEND-PROJECT/DAY6/demo.js
@@ -1,4 +1,8 @@
-let objects = {name:"aishu",age:21,dept:"csbs"}
-console.log(objects);
-objects['dept']="cse";
-console.log(objects);
+function calculate(){
+    let a=document.getElementById("button-name").value;
+    let s=0;
+    for(let i=0;i<a.length;i++){
+        s+=a.charCodeAt(i);
+    }
+    document.getElementById()
+}
